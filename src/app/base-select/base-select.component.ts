cb97d55ed7794e0f3cf19f9eaa0ac26de5a12215
kubenetic <>
2023-06-24 15:10:17 +0200

    fetch data from the server in the base component

--- src/app/base-select/base-select.component.ts
+++ src/app/base-select/base-select.component.ts
@@ -1,24 +1,31 @@
-import {Component, Input} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ControlValueAccessor} from "@angular/forms";
 import {SelectItem} from "../../model/select-item.model";
+import {Observable, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-base-select',
   templateUrl: './base-select.component.html',
   styleUrls: ['./base-select.component.scss']
 })
-export class BaseSelectComponent implements ControlValueAccessor {
+export class BaseSelectComponent implements ControlValueAccessor, OnInit, OnDestroy {
 
   value?: string;
-  onTouched = () => {};
-  onChange = (value: string) => {};
-  isDisabled = false;
+  onTouched = () => {
+  };
+  onChange = (value: string) => {
+  };
 
-  @Input() isLoading = false;
-  @Input() label?: string;
-  @Input() options!: Array<SelectItem>;
-  @Input() hint?: string;
-  @Input() error?: string;
+  isDisabled = false;
+  isLoading = false;
+  label?: string;
+  multiple = false;
+  options!: Array<SelectItem>;
+  hint?: string;
+  error?: string;
+
+  dataInitializerFn!: () => Observable<Array<SelectItem>>;
+  dataSubscription?: Subscription;
 
   registerOnChange(fn: any): void {
     this.onChange = fn;
@@ -34,6 +41,28 @@ export class BaseSelectComponent implements ControlValueAccessor {
 
   writeValue(value: any): void {
     this.value = value;
+  }
+
+  ngOnInit(): void {
+    const _this = this;
+
+    this.isLoading = true;
+    this.isDisabled = true;
+    this.dataSubscription = this.dataInitializerFn().subscribe({
+      next(values: Array<SelectItem>) {
+        _this.options = values;
+        _this.isLoading = false;
+        _this.isDisabled = false;
+      },
+      error(err) {
+        _this.error = err.error;
+        _this.isLoading = false;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.dataSubscription?.unsubscribe();
   }
 
 }
