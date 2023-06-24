a1f83d60c9489911ca884b6aa51a684f924857de
kubenetic <>
2023-06-24 14:16:23 +0200

    generate components boilerplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BaseSelectComponent } from './base-select/base-select.component';
+import { AnimalsComponent } from './animals/animals.component';
+import { FruitsComponent } from './fruits/fruits.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseSelectComponent,
+    AnimalsComponent,
+    FruitsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/base-select/base-select.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-select',
+  templateUrl: './base-select.component.html',
+  styleUrls: ['./base-select.component.scss']
+})
+export class BaseSelectComponent {
+
+}
